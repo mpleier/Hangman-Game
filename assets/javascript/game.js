2ed42ed7e780f388286f624cee1530691b1b96c0
mpleier <mpleier@gmail.com>
2017-07-26 13:46:30 -0700

    Cleaned up code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,10 @@
-
 //     console.log("working");
-
-
-var words = ["Awkward","Bagpipes","Banjo","Bungler","Croquet","Crypt","Dwarves","Fervid","Fishhook","Fjord","Gazebo","Gypsy","Haiku","Haphazard","Hyphen","Ivory","Jazzy","Jiffy","Jinx","Jukebox","Kayak","Kiosk","Klutz","Memento","Mystify","Numbskull","Ostracize","Oxygen","Pajama","Phlegm","Pixel","Polka","Quad","Quip","Rhythmic","Rogue","Sphinx","Squawk","Swivel","Toady","Twelfth","Unzip","Waxy","Wildebeest","Yacht","Zealous","Zigzag","Zippy","Zombie"];
+var words = ["Awkward", "Bagpipes", "Banjo", "Bungler", "Croquet", "Crypt", "Dwarves", "Fervid", "Fishhook", "Fjord", "Gazebo", "Gypsy", "Haiku", "Haphazard", "Hyphen", "Ivory", "Jazzy", "Jiffy", "Jinx", "Jukebox", "Kayak", "Kiosk", "Klutz", "Memento", "Mystify", "Numbskull", "Ostracize", "Oxygen", "Pajama", "Phlegm", "Pixel", "Polka", "Quad", "Quip", "Rhythmic", "Rogue", "Sphinx", "Squawk", "Swivel", "Toady", "Twelfth", "Unzip", "Waxy", "Wildebeest", "Yacht", "Zealous", "Zigzag", "Zippy", "Zombie"];
 
 var chars = [
-  "a","b","c","d","e","f","g","h","i","j",
-  "k","l","m","n","o","p","q","r","s","t",
-  "u","v","w","x","y","z"
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"
 ];
 
 var answer = randomWord();
@@ -15,28 +12,28 @@ var answer = randomWord();
 var latest = "";
 
 function restart() {
-  var answer = randomWord();
-  key.length=0;
-  key = answer.split("")
-  show.length = 0;
-  show = answer.split("");
-  show.fill("_");
+    var answer = randomWord();
+    key.length = 0;
+    key = answer.split("")
+    show.length = 0;
+    show = answer.split("");
+    show.fill("_");
 }
 
-function print(){
-  document.getElementById("body").innerHTML = ("<h1>Hangman</h1><br><br> Guess what word I\'m thinking of<br><br>Press a letter key on your keyboard to guess a letter.<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a+"<br>"+show)
+function print() {
+    document.getElementById("body").innerHTML = ("<h1>Hangman</h1><br><br> Guess what word I\'m thinking of<br><br>Press a letter key on your keyboard to guess a letter.<br><br>Wins: " + wins + "<br><br>Losses: " + losses + "<br><br>Guesses Left: " + guesses + "<br><br>Your Guesses so far: " + a + "<br>" + show)
 
 }
 
 
 function randomWord() {
-  var char1 = words[Math.floor(Math.random()*49)];
-  return ""+char1.toLowerCase();
+    var char1 = words[Math.floor(Math.random() * 49)];
+    return "" + char1.toLowerCase();
 }
 
 function randomLetter() {
-  var char1 = chars[Math.floor(Math.random()*26)];
-  return ""+char1;
+    var char1 = chars[Math.floor(Math.random() * 26)];
+    return "" + char1;
 }
 
 var key = answer.split("")
@@ -64,40 +61,38 @@ document.onkeydown = checkKey;
 function checkKey(e) {
     var event = window.event ? window.event : e;
     latest = event.key.toLowerCase();
-    key.forEach(function(item, i) { if (item == latest) show[i] = latest; });
-    if (a.indexOf(latest)<0&&chars.indexOf(latest)>0) {
+    key.forEach(function(item, i) {
+        if (item == latest) show[i] = latest;
+    });
+    if (a.indexOf(latest) < 0 && chars.indexOf(latest) > 0) {
 
-    a.push(latest);
-    guesses = guesses--;
+        a.push(latest);
+        guesses = guesses--;
 
 
-	if (key.toString(key) == show.toString(show)) {
-		wins++;
-		guesses = 9;
-		letter = randomLetter();
-		a = [];
-    z = restart();
+        if (key.toString(key) == show.toString(show)) {
+            wins++;
+            guesses = 9;
+            letter = randomLetter();
+            a = [];
+            z = restart();
+            z = print();
 
-		z = print();
+        } else if (guesses <= 1) {
+            losses++;
+            guesses = 9;
+            letter = randomLetter();
+            a = [];
+            z = restart();
+            z = print();
+        } else {
+            if (key.indexOf(latest) > -1) {
+                z = print();
+            } else {
+                guesses--
+                z = print();
+            }
 
-	}
-	else if (guesses < 1) {
-		losses++;
-		guesses = 9;
-		letter = randomLetter();
-		a = [];
-      z = restart();
-		z = print();
-	}
-	else {
-		if (key.indexOf(latest)>-1) {
-		z = print();
-		}
-    else {
-      guesses--
-      		z = print();
+        }
     }
-
-	}
-}
 }
